5d408ce58f11ea38aaf8fbd1e8437ed1fea4bfc3
Faizan Ali <faizan1041@gmail.com>
2017-03-11 22:43:15 +0500

    added whitenoise as dependency..

--- djangular/settings.py
+++ djangular/settings.py
@@ -153,3 +153,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
